46daeb64bb7cdf879015b97c524fccb43c130688
El dano <danielpliego@gmail.com>
2020-05-05 15:53:28 -0500

    fix accent

--- src/pages/volkswagen-twitter.js
+++ src/pages/volkswagen-twitter.js
@@ -71,7 +71,7 @@ const IndexPage = () => (
             20 likes. El primer mes subió la interacción un 40% y para el
             segundo mes, nuestros tweets ya tenían más de 100 likes. Para mí
             esto fue una gran experiencia pues muchas veces tenía que convencer
-            al VP para que me dejará mandar tweets que “no nos iban a aprobar” y
+            al VP para que me dejara mandar tweets que “no nos iban a aprobar” y
             que al final fueron los que más gustaron y generaron conversación.
           </p>
           <p>Aquí algunos ejemplos.</p>
